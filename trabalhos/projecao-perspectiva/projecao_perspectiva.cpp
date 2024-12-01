1618bfece0e730395361f1660df94075f1feace2
DaniloLMD <danilolmd04@gmail.com>
2024-12-01 20:16:01 -0300

    projections

--- trabalhos/projecao-perspectiva/projecao_perspectiva.cpp
+++ trabalhos/projecao-perspectiva/projecao_perspectiva.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <math.h>
 
+#define PI acos(-1)
+
 // estrutura para representar pontos 3D
 typedef struct Point {
     double x, y, z;
@@ -15,15 +17,19 @@ typedef struct Cubo {
 
     Cubo(Point centro, double tamanho);
     void Draw();
+    void Rotate(double angX, double angY, double angZ);
+    Point vertices_rotacionados[8];
 } Cubo;
 
-const int SIZE = 800; // tamanho da tela
+const int SIZE = 500; // tamanho da tela
 
 //variaveis de rotacao
-GLdouble angX = 0, angY = 0, angZ = 0; 
+GLdouble angX = 0, angY = 0, angZ = 0.0; 
 const double rotate_change = 5;
+//variaveis de translacao
 GLfloat translateX = 0.0, translateY = 0.0, translateZ = 0.0;
 GLfloat translate_change = 0.1;
+
 
 //cores disponiveis
 enum color_names {
@@ -47,6 +53,7 @@ Cubo::Cubo(Point centro, double tamanho) : centro(centro) {
     double tam = tamanho / 2.0f;
     this->tamanho = tam;
 
+    // Definição dos vértices
     vertices[0] = {centro.x - tam, centro.y - tam, centro.z - tam};
     vertices[1] = {centro.x + tam, centro.y - tam, centro.z - tam};
     vertices[2] = {centro.x + tam, centro.y + tam, centro.z - tam};
@@ -55,37 +62,81 @@ Cubo::Cubo(Point centro, double tamanho) : centro(centro) {
     vertices[5] = {centro.x + tam, centro.y - tam, centro.z + tam};
     vertices[6] = {centro.x + tam, centro.y + tam, centro.z + tam};
     vertices[7] = {centro.x - tam, centro.y + tam, centro.z + tam};
+}
 
+//projeta um ponto com perspectiva: calculamos um W diretamente proporcional à distancia do ponto
+//ao plano em que será desenhado. Então, o ponto a ser desenhado será (x/w, y/w, z)
+//Ou seja, os pontos mais longe do plano, terão coordenadas X e Y menores, gerando uma sensacao de distancia.
+void perspectiveProjection(Point& p, double left, double right, double bottom, double top, double near, double far) {
+    double w = 1.5 - p.z/2;
+    p.x *= w;
+    p.y *= w;
+    // p.z *= w;
 }
 
 
-//aplica a projecao perspectiva no ponto utilizando semelhanca de triangulo
-void perspectiveProjection(Point& p) { 
-    Point visao = {0,0,-1};
-    double visao_z = visao.z; //coordenada Z do observador
-    double plano_z = 1; //coordenada Z do plano a ser projetado
+double rad(double ang) {
+    return ang * PI /   180; 
+}
 
-    double Zp = p.z - plano_z;
-    double d = plano_z - visao_z;
+void Cubo::Rotate(double angX, double angY, double angZ) {
+    angX = rad(angX);
+    angY = rad(angY);
+    angZ = rad(angZ);
 
-    double dx = (p.x) / ((p.z/d) + 1);
-    double dy = (p.y) / ((p.z/d) + 1);
 
-    p.x = visao.x + dx;
-    p.y = visao.y + dy;
+    double cosX = cos(angX);
+    double sinX = sin(angX);
+
+    double cosY = cos(angY);
+    double sinY = sin(angY);
+
+    double cosZ = cos(angZ);
+    double sinZ = sin(angZ);
+
+
+    for (int i = 0; i < 8; i++) {
+        GLfloat x = vertices[i].x - centro.x;
+        GLfloat y = vertices[i].y - centro.y;
+        GLfloat z = vertices[i].z - centro.z;
+
+        // Rotação em torno de X
+        GLfloat tempY = y * cosX - z * sinX;
+        GLfloat tempZ = y * sinX + z * cosX;
+        y = tempY;
+        z = tempZ;
+
+        // Rotação em torno de Y
+        GLfloat tempX = x * cosY + z * sinY;
+        z = -x * sinY + z * cosY;
+        x = tempX;
+
+        // Rotação em torno de Z
+        tempX = x * cosZ - y * sinZ;
+        tempY = x * sinZ + y * cosZ;
+        x = tempX;
+        y = tempY;
+
+        vertices_rotacionados[i].x = x + centro.x;
+        vertices_rotacionados[i].y = y + centro.y;
+        vertices_rotacionados[i].z = z + centro.z;
+    }
 }
 
 // desenha o cubo
 void Cubo::Draw() {
     Point transformed[8];
 
-    //aplica a projeção perspectiva
+    Rotate(angX, angY, angZ);
+
+    //aplica a projeção ortogonal
     for (int i = 0; i < 8; ++i) {
-        transformed[i] = vertices[i];
-        transformed[i].x += translateX;
-        transformed[i].y += translateY;
-        transformed[i].z += translateZ;
-        perspectiveProjection(transformed[i]);
+        transformed
+        /*
+/projeta um ponto com perspectiva: calculamos um W diretamente proporcional à distancia do ponto
+//ao plano em que será desenhado. Então, o ponto a ser desenhado será (x/w, y/w, z)
+//Ou seja, os pontos mais longe do plano, terão coordenadas X e Y menores, gerando uma sensacao de distancia.[i] = vertices_rotacionados[i];
+        perspectiveProjection(transformed[i], -SIZE, SIZE, -SIZE, SIZE, -SIZE, SIZE);
     }
 
     //desenha as faces do cubo
@@ -134,26 +185,37 @@ void display(){
     //limpa a tela
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    //rotaciona os pontos
     glLoadIdentity();
 
-    //rotaciona os pontos
-    glRotatef(angX, 1, 0, 0);
-    glRotatef(angY, 0, 1, 0);
-    glRotatef(angZ, 0, 0, 1);
+    glTranslatef(translateX, translateY, translateZ);
 
     //deseha um cubo
-    Cubo cubo({0, 0, 0}, 0.5);
+    Cubo cubo({0, 0, 0}, 0.8);
     cubo.Draw();
 
     glutSwapBuffers();
 }
 
+//inicializa a janela e o Z-Buffer para evitar sobrescrita de pontos
+void initialize() {
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(SIZE, SIZE);
+    glutCreateWindow("Projecao Perspectiva");
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glOrtho(-1, 1, -1, 1, -1, 1);
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+}
+
+
 //lida com as setinhas do teclado(para rotacionar)
 void special_keyboard(int key, int x, int y) {
     if (key == GLUT_KEY_UP) angX += rotate_change;
     if (key == GLUT_KEY_DOWN) angX -= rotate_change;
-    if (key == GLUT_KEY_LEFT) angY -= rotate_change;
-    if (key == GLUT_KEY_RIGHT) angY += rotate_change;
+    if (key == GLUT_KEY_LEFT) angY += rotate_change;
+    if (key == GLUT_KEY_RIGHT) angY -= rotate_change;
 
     glutPostRedisplay();
 }
@@ -211,16 +273,6 @@ void keyboard(unsigned char key, int, int) {
     glutPostRedisplay();
 }
 
-//inicializa a janela e o Z-Buffer para evitar sobrescrita de pontos
-void initialize() {
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(SIZE, SIZE);
-    glutCreateWindow("Projecao Perspectiva");
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LEQUAL);
-}
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
